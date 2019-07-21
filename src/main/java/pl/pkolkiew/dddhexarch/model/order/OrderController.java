56a1b14139be38301025a3f5de955a974c382fd6
pkolkiew <pkolkiew@gmail.com>
2019-07-21 16:59:51 +0200

    - annotation in OrderController in advance

--- src/main/java/pl/pkolkiew/dddhexarch/model/order/OrderController.java
+++
@@ -1,8 +0,0 @@
-package pl.pkolkiew.dddhexarch.model.order;
-
-/**
- * @author pkolkiew
- * Created 20.07.2019
- */
-class OrderController {
-}
---
+++ src/main/java/pl/pkolkiew/dddhexarch/model/order/domain/infrastructure/OrderController.java
@@ -0,0 +1,16 @@
+package pl.pkolkiew.dddhexarch.model.order.domain.infrastructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pkolkiew
+ * Created 20.07.2019
+ */
+@Slf4j
+@RestController
+@RequestMapping(path = "/order")
+class OrderController {
+
+}
